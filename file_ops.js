cf6317207b5ed3a7bbbf9b84af4e72aab190744d
Jason Burgett <jasbur@gmail.com>
2013-01-20 10:15:17 -0600

    Added proper file type sorting using Arrays

--- file_ops.js
+++ file_ops.js
@@ -3,19 +3,31 @@ var fs = require('fs');
 
 function file_scan(){
 	var findFiles = exec("find /media/customerdrive/ -type f > full_file_list");
+	var exeFiles = new Array();
+	var dllFiles = new Array();
+	var sysFiles = new Array();
+	var drvFiles = new Array();
 	
 	findFiles.on('exit', function(){
 		var fullFileList = fs.readFileSync("full_file_list", 'utf8');
 		var lines = fullFileList.split("\n");
 		
-		var numFiles = 0;
 		for(l in lines){
-			if(lines[l].indexOf("exe") > -1){
-				numFiles++;
+			if(lines[l].indexOf(".exe") > -1){
+				exeFiles.push(lines[l]);
+			}else if(lines[l].indexOf(".dll") > -1){
+				dllFiles.push(lines[l]);
+			}else if(lines[l].indexOf(".sys") > -1){
+				sysFiles.push(lines[l]);
+			}else if(lines[l].indexOf(".drv") > -1){
+				drvFiles.push(lines[l]);
 			}
 		}
 		
-		console.log(numFiles + " .exe files");
+		console.log(exeFiles.length + " .exe files");
+		console.log(dllFiles.length + " .dll files");
+		console.log(sysFiles.length + " .sys files");
+		console.log(drvFiles.length + " .drv files");
 	});
 }
 
