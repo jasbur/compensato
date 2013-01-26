e8b4871ec541fc61c060c9fa7c20252cf6813a1f
Jason Burgett <jasbur@gmail.com>
2013-01-26 14:32:23 -0600

    Added some commenting to the createScanLog function

--- output.js
+++ output.js
@@ -6,12 +6,14 @@ module.exports = {
 			console.log("\u001B[2J\u001B[0;0f");
 		},
 
+		//Creates scan_log.txt from fullFileList
 		createScanLog: function(lines, extensions){
 			fs.writeFileSync('./scan_log.txt', "");
 			
 			for(e in extensions){
 				createScanLogHeader(extensions[e]);
 				
+				//If the current line contains the current extension, write it to scan_log.txt
 				for (l in lines){					
 					if(lines[l].indexOf(extensions[e]) > -1){
 						fs.appendFileSync('./scan_log.txt', lines[l] + "\n");
@@ -22,6 +24,7 @@ module.exports = {
 		
 };
 
+//Creates the header for each extension in scan_log.txt
 function createScanLogHeader(extension){
 	fs.appendFileSync('./scan_log.txt', "\n");
 	fs.appendFileSync('./scan_log.txt', "****************************************\n");
