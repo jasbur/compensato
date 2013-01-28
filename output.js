7533696fe5be0aaf610dbd2e5729d3c88644cfb0
Jason Burgett <jasbur@gmail.com>
2013-01-27 19:25:25 -0600

    Re-wrote the scan_log.txt creation to use proper flow control. We now combine to whole log into an array before writing it to the file all at once

--- output.js
+++ output.js
@@ -8,35 +8,47 @@ module.exports = {
 
 		//Creates scan_log.txt from fullFileList
 		createScanLog: function(lines, extensions){
-			fs.writeFileSync('./scan_log.txt', "");
+			var linesToWrite = new Array();
 			
-			for(e in extensions){
-				createScanLogHeader(extensions[e]);
-				
-				//If the current line contains the current extension, write it to scan_log.txt
-				for (l in lines){					
-					if(lines[l].indexOf(extensions[e]) > -1){
-						getFileProperty(lines[l], "mtime", function(path, prop){
-							fs.appendFileSync('./scan_log.txt', prop + " " + path + "\n");
-						});
-					}
-				}
-			}
+			createScanLogHeader(lines, extensions, linesToWrite);
 		}
 		
 };
 
 //Creates the header for each extension in scan_log.txt
-function createScanLogHeader(extension){
-	fs.appendFileSync('./scan_log.txt', "\n");
-	fs.appendFileSync('./scan_log.txt', "****************************************\n");
-	fs.appendFileSync('./scan_log.txt', "***** Recently modified " + extension + " files *****\n");
-	fs.appendFileSync('./scan_log.txt', "****************************************\n");
-	fs.appendFileSync('./scan_log.txt', "\n");
+function createScanLogHeader(lines, extensions, linesToWrite, fn){
+	for(e in extensions){
+		linesToWrite.push("\n");
+		linesToWrite.push("****************************************\n");
+		linesToWrite.push("***** Recently modified " + extensions[e] + " files *****\n");
+		linesToWrite.push("****************************************\n");
+		linesToWrite.push("\n");
+		checkForValidFiles(lines, extensions[e], linesToWrite);
+	}
+	writeLinesToFile(linesToWrite);
+}
+
+function checkForValidFiles(lines, extension, linesToWrite){
+	//If the current line contains the current extension, write it to scan_log.txt
+	for (l in lines){					
+		if(lines[l].indexOf(extension) > -1){
+			getFileProperty(lines[l], "mtime", function(path, prop){
+				////////////////////
+			});
+			linesToWrite.push(lines[l] + "\n");
+		}
+	}
 }
 
 function getFileProperty(path, prop, fn){
 	fs.stat(path, function(err, stats){
 		fn(path, stats[prop]);
 	});
+}
+
+function writeLinesToFile(linesToWrite){
+	fs.writeFileSync('./scan_log.txt', "");
+	for(line in linesToWrite){
+		fs.appendFileSync('./scan_log.txt', linesToWrite[line]);
+	}
 }
