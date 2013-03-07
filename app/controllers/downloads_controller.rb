bab99e58d9c15cc0198b930f71373fc31f1d6001
jasbur <jasbur@gmail.com>
2013-03-07 10:35:14 -0600

    /download/index now checks to make sure there is an active network connection to use before parsing the ComboFix url and generates variables. It also displays an appropriate message if you are offline

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,18 +2,28 @@ class DownloadsController < ApplicationController
 
 	#Main index for the "downloads" controller. Also generates download links to be selected in index view
 	def index
-		combofix_url = Download.get_combofix_url
-		@file_links = {
-			"ComboFix" => combofix_url, 
-			"TDSSKiller" => "http://support.kaspersky.com/downloads/utils/tdsskiller.exe",
-			"Full Compensato Suite (Chrome, MSSE, Flash, Java, Malwarebytes, Adobe Reader)" => "http://ninite.com/chrome-essentials-flash-flashie-java-malwarebytes-reader/ninite.exe",
-			"Malwarebytes" => "http://ninite.com/malwarebytes/ninite.exe",
-			"Google Chrome" => "http://ninite.com/chrome/ninite.exe",
-			"Microsoft Security Essentials" => "http://ninite.com/essentials/ninite.exe",
-			"CCleaner" => "http://download.piriform.com/ccsetup327.exe",
-			"Adobe Flash" => "http://ninite.com/flash-flashie/ninite.exe",
-			"Java" => "http://ninite.com/java/ninite.exe"
-		}
+		@online_status = true
+
+		if SystemInfo.get_system_stats[5].nil?
+			@online_status = false
+		end
+
+		if @online_status == true
+			combofix_url = Download.get_combofix_url
+
+			@file_links = {
+				"ComboFix" => combofix_url, 
+				"TDSSKiller" => "http://support.kaspersky.com/downloads/utils/tdsskiller.exe",
+				"Full Compensato Suite (Chrome, MSSE, Flash, Java, Malwarebytes, Adobe Reader)" => "http://ninite.com/chrome-essentials-flash-flashie-java-malwarebytes-reader/ninite.exe",
+				"Malwarebytes" => "http://ninite.com/malwarebytes/ninite.exe",
+				"Google Chrome" => "http://ninite.com/chrome/ninite.exe",
+				"Microsoft Security Essentials" => "http://ninite.com/essentials/ninite.exe",
+				"CCleaner" => "http://download.piriform.com/ccsetup327.exe",
+				"Adobe Flash" => "http://ninite.com/flash-flashie/ninite.exe",
+				"Java" => "http://ninite.com/java/ninite.exe"
+			}
+
+		end
 	end
 
 	#Process the selected downloads from /downloads/index
