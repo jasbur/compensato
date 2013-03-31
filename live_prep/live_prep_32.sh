d30e7c9a285ae1a8697780511ff4dd11aa76a20c
Jason Burgett <jasbur@gmail.com>
2013-03-30 21:09:48 -0500

    All live scripting should be done except for some visual elements right at the boot choice menu. Some initial groundwork for that has been laid in the after.prep.sh script though

--- live_prep/live_prep_32.sh
+++ live_prep/live_prep_32.sh
@@ -3,13 +3,13 @@
 #Copy new sources.list to /etc/apt
 cp sources.list /etc/apt/
 
-#Copy grub configuration file to /etc/default
-cp grub /etc/default/
-
 #Install dependencies
 apt-get update
 apt-get upgrade -y
 apt-get install rails3 curl smartmontools wine gdisk -y
+
+#Remove unwanted packages
+apt-get remove ubiquity -y
 
 #Copy the launch script
 cp launch_compensato.sh /home/
--- live_prep/live_prep_64.sh
+++ live_prep/live_prep_64.sh
@@ -3,9 +3,6 @@
 #Copy new sources.list to /etc/apt
 cp sources.list /etc/apt/
 
-#Copy grub configuration file to /etc/default
-cp grub /etc/default/
-
 #This is to bypass some bug in Ubuntu 12.10 that doesn't allow wine to install properly otherwise
 sudo dpkg --add-architecture i386
 
@@ -13,6 +10,9 @@ sudo dpkg --add-architecture i386
 apt-get update
 apt-get upgrade -y
 apt-get install rails3 curl smartmontools wine gdisk -y
+
+#Remove unwanted packages
+apt-get remove ubiquity -y
 
 #Copy the launch script
 cp launch_compensato.sh /home/
