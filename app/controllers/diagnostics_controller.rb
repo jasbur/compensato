58074a5e8901110ac2d1e6fd062373a7a2cda6a1
Jason Burgett <jasbur@gmail.com>
2013-04-09 15:53:52 -0500

    Initial attempt to see if code works with hard-coded values

--- app/controllers/diagnostics_controller.rb
+++ app/controllers/diagnostics_controller.rb
@@ -10,7 +10,7 @@ class DiagnosticsController < ApplicationController
 		if @diagnostic_type == "bluescreen_view"
 			spawn "sudo -u ubuntu wine ext_apps/bluescreenview/BlueScreenView.exe /MiniDumpFolder z:\\\\media\\\\compensato_client\\\\Windows\\\\Minidump"
 		elsif @diagnostic_type == "network_health_test"
-			
+			@ping_results = Diagnostic.ping_test(100)
 		end
 	end
 
--- app/models/diagnostic.rb
+++ app/models/diagnostic.rb
@@ -1,5 +1,21 @@
 class Diagnostic
 
-	
-	
+	def self.ping_test(number_of_pings)
+		ping_results = Hash.new
+
+		these_results = %x(ping -fq 192.168.1.1 -c #{number_of_pings}).split("\n")
+
+		these_results.each{|line|
+			if line.include?("packet loss")
+				ping_results.merge!(:packet_loss_percentage => line.split(" ")[5][0..-2])
+			elsif line.include?("rtt")
+				ping_results.merge!(:rtt_min => line.split("/")[3][7..-1])
+				ping_results.merge!(:rtt_avg => line.split("/")[4])
+				ping_results.merge!(:rtt_max => line.split("/")[5])
+			end
+		}
+
+		return ping_results
+	end
+
 end
