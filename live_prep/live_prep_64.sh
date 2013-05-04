62e37e841a0578cf0e34417b4671956e2d38e208
Jason Burgett <jasbur@gmail.com>
2013-05-04 14:19:13 -0500

    Correction on the launcher link

--- live_prep/live_prep_64.sh
+++ live_prep/live_prep_64.sh
@@ -45,7 +45,7 @@ ln -sf ../ext_apps/mprime_statics/mprime64 ../ext_apps/mprime
 
 #Set the script to auto-run on login
 mkdir -p /home/ubuntu/Desktop
-ln -sf /usr/bin/launch_compensato.sh /home/ubuntu/Desktop/Launch\ Compensato
+ln -sf /usr/bin/compensato.sh /home/ubuntu/Desktop/Launch\ Compensato
 
 #Change all permissions to 777 in /home/ubuntu
 chmod -R 777 /home/ubuntu
