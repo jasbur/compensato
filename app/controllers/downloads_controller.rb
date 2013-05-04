04dfa277d2c0a8ad1529471ddfa038b93af201c3
Jason Burgett <jasbur@gmail.com>
2013-05-04 09:18:51 -0500

    Removed some unecessary download links from the download page

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -12,14 +12,11 @@ class DownloadsController < ApplicationController
 			combofix_url = Download.get_combofix_url
 
 			@file_links = {
-				"Full Compensato Suite (Chrome, MSSE, Flash, Java, Malwarebytes, Adobe Reader)" => "http://ninite.com/chrome-essentials-flash-flashie-java-malwarebytes-reader/ninite.exe",
 				"TDSSKiller" => "http://support.kaspersky.com/downloads/utils/tdsskiller.exe",
-				"Malwarebytes" => "http://ninite.com/malwarebytes/ninite.exe",
 				"ComboFix" => combofix_url,
+				"Malwarebytes" => "http://ninite.com/malwarebytes/ninite.exe",
 				"Google Chrome" => "http://ninite.com/chrome/ninite.exe",
-				"Microsoft Security Essentials" => "http://ninite.com/essentials/ninite.exe",
-				"Adobe Flash" => "http://ninite.com/flash-flashie/ninite.exe",
-				"Java" => "http://ninite.com/java/ninite.exe"
+				"Microsoft Security Essentials" => "http://ninite.com/essentials/ninite.exe"
 			}
 
 		end
