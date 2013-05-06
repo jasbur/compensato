1f873daab09528c88cf7dc160741662a7e82742c
Jason Burgett <jasbur@gmail.com>
2013-05-06 09:48:03 -0500

    Permanently removed the rc.local config file and copy line in the live build because the new start-up method makes it no longer necessary

--- live_prep/live_prep_32.sh
+++ live_prep/live_prep_32.sh
@@ -20,9 +20,6 @@ cp ./conf_files/launch_compensato_server.sh /usr/bin/
 #Copy the launch script
 cp ./conf_files/compensato.sh /usr/bin
 
-#Copy rc.local to start the rails server on boot
-#cp ./conf_files/rc.local /etc
-
 #Copy the .desktop file to autostart and run the /usr/bin/compensato.sh script when logging in
 mkdir -p /home/ubuntu/.config/autostart
 cp ./conf_files/Launch_Compensato.desktop /home/ubuntu/.config/autostart
