40113bfab3f2286b358a70600604f3f82864827a
Jason Burgett <jasbur@gmail.com>
2013-08-18 22:06:03 -0700

    Live build now copies the .desktop file to the Desktop instead of creating a symbolic link to the .sh file

--- live_prep/live_prep_32.sh
+++ live_prep/live_prep_32.sh
@@ -41,7 +41,7 @@ ln -sf ../ext_apps/mprime_statics/mprime32 ../ext_apps/mprime
 
 #Set the script to auto-run on login
 mkdir -p /home/ubuntu/Desktop
-ln -sf /usr/bin/compensato.sh /home/ubuntu/Desktop/Launch\ Compensato
+cp ./conf_files/Launch_Compensato.desktop /home/ubuntu/Desktop/
 
 #Copy Unity launcher favorites
 mkdir -p /home/ubuntu/.config/dconf
